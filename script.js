b0e90dbc01118bbfce0ca49ff0fbdd0b0e7e5fc0
unknown <nae.imuam103@gmail.com>
2022-10-01 17:31:47 +0530

    UpdATE my profile look.

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+var bgc = '#fff';
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+// Create gradient
+
+var grd = ctx.createLinearGradient(0,200,200,0);
+grd.addColorStop(0,"rgb(205, 51, 255)");
+grd.addColorStop(.5,"rgb(255, 51, 52)");
+grd.addColorStop(1,"rgb(255, 204, 51)");
+
+var img = document.getElementById("img");
+// Fill with gradient
+ctx.fillStyle = grd;
+
+ctx.beginPath();
+ctx.arc(105, 105, 105, 0, 2 * Math.PI);
+ctx.fill();
+ctx.beginPath();
+ctx.arc(105, 105, 99, 0, 2 * Math.PI);
+ctx.fillStyle = bgc;
+ctx.fill();
+
+ctx.save();
+ctx.beginPath();
+ctx.arc(105, 105, 95, 0, 2*Math.PI, false);
+ctx.clip();
+ctx.drawImage(img, 5, 0, 200, 218);
+ctx.restore();
+
+
+var c = document.getElementById("myCanvas0");
+var ctx = c.getContext("2d");
+
+// Create gradient
+
+var grd = ctx.createLinearGradient(0,30,30,0);
+grd.addColorStop(0,"rgb(205, 51, 255)");
+grd.addColorStop(.5,"rgb(255, 51, 52)");
+grd.addColorStop(1,"rgb(255, 204, 51)");
+
+var img = document.getElementById("img");
+// Fill with gradient
+ctx.fillStyle = grd;
+
+ctx.beginPath();
+ctx.arc(30, 30, 30, 0, 2 * Math.PI);
+ctx.fill();
+ctx.beginPath();
+ctx.arc(30, 30, 28, 0, 2 * Math.PI);
+ctx.fillStyle = bgc;
+ctx.fill();
+
+ctx.save();
+ctx.beginPath();
+ctx.arc(30, 30, 26, 0, 2*Math.PI, false);
+ctx.clip();
+ctx.drawImage(img, 2, 0, 60, 65);
+ctx.restore();
+
+
 let theme = localStorage.getItem('theme');
 
 if(theme == null){
@@ -39,33 +100,8 @@ function setTheme(mode){
 
   localStorage.setItem('theme', mode)
 
+}
 
-  var c = document.getElementById("myCanvas0");
-  var ctx = c.getContext("2d");
-
-  // Create gradient
-
-  var grd = ctx.createLinearGradient(0,30,30,0);
-  grd.addColorStop(0,"rgb(205, 51, 255)");
-  grd.addColorStop(.5,"rgb(255, 51, 52)");
-  grd.addColorStop(1,"rgb(255, 204, 51)");
-
-  var img = document.getElementById("profile_pic");
-  // Fill with gradient
-  ctx.fillStyle = grd;
-
-  ctx.beginPath();
-  ctx.arc(30, 30, 30, 0, 2 * Math.PI);
-  ctx.fill();
-  ctx.beginPath();
-  ctx.arc(30, 30, 28, 0, 2 * Math.PI);
-  ctx.fillStyle = bgc;
-  ctx.fill();
-
-  ctx.save();
-  ctx.beginPath();
-  ctx.arc(30, 30, 26, 0, 2*Math.PI, false);
-  ctx.clip();
-  ctx.drawImage(img, 2, 0, 60, 65);
-  ctx.restore();
+function fun(){
+  alert("I CAN DESIGN YOUR SITE UPTO NEXT LEVEL.")
 }
